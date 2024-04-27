dcf7091a2ae43e3174e40d784c1a641356395edf
Maftuna <svmca19@gmail.com>
2024-04-28 00:23:27 +0500

    masala

---
+++ main.py
@@ -0,0 +1,68 @@
+#Task1
+# def lst():
+#     lst1 =[1,2,3]
+#     lst2 =[80,5,100]
+#     lst3 = [-500,0,50]
+#     return lst1[0],lst2[0],lst3[0]
+#
+# print(lst())
+
+#Task2
+#
+# contact =[1,3,5],[2,6,8]
+# print(contact[0] + contact[1])
+#
+# contact1 = [7,8],[10,9,1,1,2]
+# print(contact1[0] + contact1[1])
+#
+# contact2 = [4,5,1],[3,3,3,3,3]
+# print(contact2[0] + contact2[1])
+
+# def contacts():
+#     contact = [1, 3, 5], [2, 6, 8]
+#
+#     contact1 = [7, 8], [10, 9, 1, 1, 2]
+#
+#     contact2 = [4, 5, 1], [3, 3, 3, 3, 3]
+#     return contact[0] + contact[1], contact1[0] + contact1[1],contact2[0] + contact2[1]
+#
+# print(contacts())
+
+#Task3
+# def lsts():
+#     lst = [1,2,3]
+#     lst1 = ["cat","dog","duck"]
+#     lst2 = [True,False,True]
+#     lst3 = [7,"String",False]
+#     all = lst[-1],lst1[-1],lst2[-1],lst3[-1]
+#     return all
+# print(lsts())
+
+#Task4
+# def ints():
+#     lst = []
+#     lst1 = []
+#     lst2 = []
+#     make_pair = 1,2
+#     make_pair1 = 51,21
+#     make_pair2 = 512124,215
+#     lst.extend(make_pair)
+#     lst1.extend(make_pair1)
+#     lst2.extend(make_pair2)
+#     return lst,  lst1, lst2
+# print(ints())
+
+# lst = []
+# make_pair = 1, 2
+# lst.extend(make_pair)
+# print(lst)
+#
+# lst1 = []
+# make_pair1 = 51, 21
+# lst1.extend(make_pair1)
+# print(lst1)
+#
+# lst2 = []
+# make_pair2 = 512124,215
+# lst2.extend(make_pair2)
+# print(lst2)
